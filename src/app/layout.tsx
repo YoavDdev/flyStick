bee70d2159d4fef3796332b2a7adb1acfedf055a
Yoav Drai <yoavddev@gmail.com>
2023-11-28 20:32:07 +0200

    favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,12 @@ export const metadata: Metadata = {
   description:
     "Dive into a Vast Collection of Lessons and Exercises, Expertly Curated to Enhance Your Body-Mind Connection and Elevate Your Well-Being with Varied Training Techniques and Difficulty Levels.",
   keywords: ["Flystick"],
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
