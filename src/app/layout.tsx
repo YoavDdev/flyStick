994cf87c4883aab67d8d674278a21c7150e1f461
Yoav Drai <yoavddev@gmail.com>
2023-11-28 22:12:37 +0200

    favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export const metadata: Metadata = {
     apple: ["/apple-touch-icon.png?v=4"],
     shortcut: ["/apple-touch-icon.png"],
   },
+  openGraph: {
+    images: "/android-chrome-144x144.png",
+  },
   manifest: "/site.webmanifest",
 };
 
