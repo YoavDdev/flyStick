133d729bb6128de8fd8aad85cb7b65f6b0cded2d
Yoav Drai <yoavddev@gmail.com>
2023-12-02 16:48:54 +0200

    forgot password

--- src/app/api/activate-reset-password/route.jsx
+++ src/app/api/activate-reset-password/route.jsx
@@ -4,51 +4,45 @@ import bcrypt from "bcrypt";
 import prisma from "../../libs/prismadb";
 import { NextResponse } from "next/server";
 
-export async function handler(request) {
-  if (request.method === "PATCH") {
-    try {
-      const body = await request.json();
-      const { token, password } = body;
+export async function PATCH(request) {
+  try {
+    const body = await request.json();
+    const { token, password } = body;
 
-      // Validate token, check expiration, etc. (Add your validation logic here)
-      const resetInfo = await prisma.passwordReset.findFirst({
-        where: {
-          token,
-        },
-        include: {
-          user: true,
-        },
-      });
+    // Validate token, check expiration, etc. (Add your validation logic here)
+    const resetInfo = await prisma.passwordReset.findFirst({
+      where: {
+        token,
+      },
+      include: {
+        user: true,
+      },
+    });
 
-      if (!resetInfo || resetInfo.expiresAt < new Date()) {
-        return new NextResponse("Invalid or expired token", { status: 400 });
-      }
-
-      // Update user's password
-      const hashedPassword = await bcrypt.hash(password, 10);
-      await prisma.user.update({
-        where: {
-          id: resetInfo.userId,
-        },
-        data: { hashedPassword },
-      });
-
-      // Delete the used reset token
-      await prisma.passwordReset.delete({
-        where: {
-          id: resetInfo.id, // Assuming resetInfo has the id field
-          token: resetInfo.token,
-        },
-      });
-
-      return new NextResponse({ message: "Password reset successful" });
-    } catch (error) {
-      console.error("Error completing password reset:", error);
-      return new NextResponse("Internal Server Error", { status: 500 });
+    if (!resetInfo || resetInfo.expiresAt < new Date()) {
+      return new NextResponse("Invalid or expired token", { status: 400 });
     }
-  } else {
-    return new NextResponse("Method Not Allowed", { status: 405 });
+
+    // Update user's password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await prisma.user.update({
+      where: {
+        id: resetInfo.userId,
+      },
+      data: { hashedPassword },
+    });
+
+    // Delete the used reset token
+    await prisma.passwordReset.delete({
+      where: {
+        id: resetInfo.id, // Assuming resetInfo has the id field
+        token: resetInfo.token,
+      },
+    });
+
+    return new NextResponse({ message: "Password reset successful" });
+  } catch (error) {
+    console.error("Error completing password reset:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
-
-export { handler as PATCH, handler as POST };
