af4a2a9b8391049cec6d83d85bcda775c2128546
Yoav Drai <yoavddev@gmail.com>
2024-01-07 13:03:12 +0200

    boaz new update 7/1

--- src/app/components/Hero2.tsx
+++ src/app/components/Hero2.tsx
@@ -1,30 +1,50 @@
 "use client";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import BoazMain_Clean from "../../../public/BoazMain_Clean.png";
 import SubscriptionDetails from "./../api/SubscriptionDetails";
 import new_main from "../../../public/new_main.png";
 
 const Hero = () => {
-  const [visible, setVisible] = useState(true); // Set initial state to false
+  const [visible, setVisible] = useState(false);
   const { subscriptionStatus, loading } = SubscriptionDetails();
 
+  useEffect(() => {
+    // Set a delay to trigger the fade-in animation
+    const delay = setTimeout(() => {
+      setVisible(true);
+    }, 500);
+
+    // Clear the timeout to avoid memory leaks
+    return () => clearTimeout(delay);
+  }, []);
   return (
-    <div className="relative mt-20">
-      <div className="overflow-hidden rounded-md">
+    <div className="relative mt-32 sm:mt-10">
+      <div
+        className={`overflow-hidden rounded-md transform ${
+          visible
+            ? "opacity-100 scale-100 transition-opacity duration-1000 ease-in-out delay-500"
+            : "opacity-0 scale-80"
+        }`}
+      >
         <Image
           src={new_main}
           alt="BoazMain"
           layout="responsive"
-          width={1920}
-          height={1080}
+          className="object-cover w-full h-full rounded-lg"
+          priority
         />
       </div>
-
-      <div className="absolute inset-0 flex items-center justify-center text-center text-white lg:mb-32">
+      <div
+        className={`absolute inset-0 flex items-center justify-center text-center text-white lg:mb-32 ${
+          visible
+            ? "opacity-100 transition-opacity duration-1000 ease-in-out"
+            : "opacity-0"
+        }`}
+      >
         <div className="mx-auto max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl mb-4">
+          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl mb-4 text-gray-700 sm:text-white">
             Boaz Nahaisi&apos;s Online Studio
           </h1>
           <p className="mt-6 text-gray-700 sm:text-xl mx-5">
@@ -38,14 +58,14 @@ const Hero = () => {
               </>
             ) : (
               <>
-                Dive into a vast collection of lessons and exercises, expertly
-                curated to enhance your body-mind connection and elevate your
-                well-being with varied training techniques and difficulty
-                levels.
+                <span className="text-white ">Dive into a vast </span>{" "}
+                collection of lessons and exercises, expertly curated to enhance
+                your body-mind connection and elevate your well-being with
+                varied training techniques and difficulty levels.
               </>
             )}
           </p>
-          <div className="mt-10 flex items-center justify-center transition-all duration-500 ease-in-out">
+          <div className="mt-5 flex flex-col sm:flex-row items-center justify-center transition-all duration-500 ease-in-out">
             {loading ? (
               <p>Loading...</p>
             ) : subscriptionStatus === "ACTIVE" ? (
@@ -58,7 +78,9 @@ const Hero = () => {
             ) : (
               <Link
                 href="/register"
-                className="rounded-full bg-[#2D3142] px-6 py-3 text-lg text-white shadow-lg hover:bg-[#4F5D75] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-transform transform hover:scale-105"
+                className={`rounded-full bg-[#2D3142] px-6 py-3 text-lg text-white shadow-lg hover:bg-[#4F5D75] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-opacity transform hover:scale-105 ${
+                  visible ? "opacity-100" : "opacity-0"
+                }`}
               >
                 Start Your 3-Day Free Trial
               </Link>
