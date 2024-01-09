f90f29445c0f9aed9cd0cb6e1894bfc5a6f9f98a
Yoav Drai <yoavddev@gmail.com>
2024-01-09 14:00:43 +0200

    boaz new update 9/1

--- src/app/components/Hero2.tsx
+++ src/app/components/Hero2.tsx
@@ -20,31 +20,29 @@ const Hero = () => {
     return () => clearTimeout(delay);
   }, []);
   return (
-    <div className="relative object-contain bg-auto mt-20">
+    <div className="relative isolate overflow-hidden pt-36 sm:pt-64 md:h-screen">
       <div
-        className={`overflow-hidden transform ${
-          visible
-            ? "opacity-100 scale-100 transition-opacity duration-1000 ease-in-out delay-500"
-            : "opacity-0 scale-80"
+        className={`absolute inset-0 -z-10 h-full w-full object-cover transition-opacity ${
+          visible ? "opacity-100" : "opacity-0"
         }`}
       >
         <Image
           src={new_main}
           alt="BoazMain"
-          layout="responsive"
-          className="object-cover"
+          layout="fill"
+          className="object-cover mt-20"
           priority
         />
       </div>
       <div
-        className={`absolute inset-0 flex items-center justify-center text-center text-white lg:mb-32 ${
-          visible
-            ? "opacity-100 transition-opacity duration-1000 ease-in-out"
-            : "opacity-0"
-        }`}
+        className={`absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80`}
+        aria-hidden="true"
       >
-        <div className="mx-auto max-w-2xl mt-20 sm:mt-0">
-          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl mb-4  sm:text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        <div />
+      </div>
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 ">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
             Boaz Nahaisi&apos;s Online Studio
           </h1>
           <p className="mt-6 text-gray-700 sm:text-xl mx-5">
@@ -87,6 +85,18 @@ const Hero = () => {
             )}
           </div>
         </div>
+      </div>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
       </div>
     </div>
   );
