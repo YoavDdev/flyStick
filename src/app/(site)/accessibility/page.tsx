fce36b600561a10aa63b825c63480e64f37824f2
Yoav Drai <yoavddev@gmail.com>
2024-08-21 19:34:43 +0300

    boaz new update hebrew + terms & accessacility & privacyPolicy 3

--- src/app/(site)/accessibility/page.tsx
+++ src/app/(site)/accessibility/page.tsx
@@ -10,7 +10,7 @@ const AccessibilityStatement = () => {
               <div className="relative z-[1] block rounded-lg px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] backdrop-blur-[30px] dark:shadow-black/20 md:px-12 bg-[#FCF6F5] text-right">
                 <h2 className="mb-12 text-3xl font-bold">הצהרת נגישות</h2>
                 <p className="text-lg text-gray-700 mb-8">
-                  באתר &quot;Boaz Nahaissi&apos;s Online Studio&quot;, אנו שמים
+                  באתר , אנו שמים
                   דגש רב על נגישות ומאמינים כי כל משתמש, עם כל מגבלה שהיא, זכאי
                   לקבל גישה שווה לתכנים ושירותים המוצעים באתר. אנו פועלים לשיפור
                   מתמיד של נגישות האתר מתוך רצון לאפשר לכל המשתמשים, כולל אנשים
